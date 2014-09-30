060d9ccfb6bd4fd9831efcb7cc89717c549fc498
simendag <s236376@stud.hioa.no>
2014-09-30 12:28:54 +0200

    La til en TODO

--- Simens forslag/UnikeTalltest.java
+++ Simens forslag/UnikeTalltest.java
@@ -1,7 +1,8 @@
 public class UnikeTalltest {
 
 	public static void main(String... args) {
-
+		
+		// TODO: Erstatt 900 enten med fornuftig parameter eller input dialog
 		UnikeTall u = new UnikeTall(900);
 
 		u.arrayTest();
